67d9467f676f26a3964598048db8d6363c2123fc
Anky Baito Y <baitoyawanis@gmail.com>
2019-11-22 05:08:38 +1100

    init

---
+++ src/main/java/org/baito/h2access/H2Access.java
@@ -0,0 +1,42 @@
+package org.baito.h2access;
+
+import org.h2.Driver;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class H2Access {
+    private java.sql.Connection conn;
+
+    public H2Access(){};
+
+    public H2Access(String url, String user, String pass) throws SQLException, ClassNotFoundException {
+        connect(url, user, pass);
+    }
+
+    public void connect(String url, String user, String pass) throws SQLException, ClassNotFoundException {
+        Class.forName("org.h2.Driver");
+        url = verifyDirectory(url);
+        conn = DriverManager.getConnection(url,user,pass);
+        //Driver.load().connect();
+    }
+
+    public ResultSet query(String query) throws SQLException {
+        return conn.createStatement().executeQuery(query);
+    }
+
+    public void update(String update) throws SQLException {
+        conn.createStatement().executeUpdate(update);
+    }
+
+    public void lupdate(String update) throws SQLException {
+        conn.createStatement().executeLargeUpdate(update);
+    }
+
+    private String verifyDirectory(String path) {
+        return path.contains("jdbc:h2:") ? path : "jdbc:h2:" + path;
+    }
+}
---
+++ src/main/java/org/baito/h2access/Main.java
@@ -0,0 +1,19 @@
+package org.baito.h2access;
+
+import net.minecraftforge.fml.common.Mod;
+
+import java.sql.*;
+
+@Mod(
+        modid = Main.MOD_ID,
+        name = Main.MOD_NAME,
+        version = Main.VERSION,
+        acceptableRemoteVersions = "*"
+)
+public class Main {
+
+    public static final String MOD_ID = "h2access";
+    public static final String MOD_NAME = "H2access";
+    public static final String VERSION = "2019.1-1.2.23";
+    
+}
